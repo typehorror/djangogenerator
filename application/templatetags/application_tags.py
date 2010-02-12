ca7d7626bee1a25ba2ed9f47845b83aecb3c100c
Brice Leroy <bbrriiccee@gmail.com>
2010-02-11 16:21:11 -0800

    Fix application templatetags

--- application/templatetags/application_tags.py
+++ application/templatetags/application_tags.py
@@ -9,10 +9,10 @@ def application_view(application):
 
 @register.inclusion_tag('application_form.html')
 def application_form(application):
-    application_form = ApplicationForm(instance=application, prefix='app_%d_' % application.id)
+    application_form = ApplicationForm(application.project, instance=application, prefix='app_%d_' % application.id)
     return {'application': application, 'application_form': application_form}
 
 @register.inclusion_tag('new_application_form.html')
 def new_application_form(project):
-	new_application_form = NewApplicationForm(prefix='new_application_')
+	new_application_form = NewApplicationForm(project, prefix='new_application_')
 	return {'new_application_form': new_application_form, 'project':project}
